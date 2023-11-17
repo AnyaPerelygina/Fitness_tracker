b99472f458b6eda173716b43f28746cb6bfc872d
Anna <mynameisannanas@gmail.com>
2023-11-18 00:18:54 +0100

    feat: pricing

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {createAboutSlider} from './modules/about';
+import {initTabs} from './modules/tabs/init-tabs.js';
 
 // ---------------------------------
 
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  initTabs();
 
   // Modules
   // ---------------------------------

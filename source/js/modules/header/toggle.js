75ae2961fe231078f6ba13e559850ceff496edc8
Anna <mynameisannanas@gmail.com>
2023-11-23 21:19:39 +0100

    fix: js toggle

--- source/js/modules/header/toggle.js
+++ source/js/modules/header/toggle.js
@@ -1,11 +1,16 @@
 const addToggleMenu = () => {
   const OPENED_CLASS = 'is-opened';
+  const LINK_CLASS = '.nav__link';
   const root = document.querySelector('.header');
   const toggle = root.querySelector('.toggle');
   const nav = root.querySelector('.nav');
 
   const onDocumentKeydown = (evt) => {
     return evt.key === 'Escape' ? closeMenu() : null;
+  };
+
+  const onLinkClick = (evt) => {
+    return evt.target.matches(LINK_CLASS) ? closeMenu() : null;
   };
 
   const isMenu = (evt) => {
@@ -17,9 +22,11 @@ const addToggleMenu = () => {
     toggle.classList.add(OPENED_CLASS);
     nav.classList.add(OPENED_CLASS);
     document.addEventListener('keydown', onDocumentKeydown);
+    nav.addEventListener('click', onLinkClick);
     document.addEventListener('click', isMenu);
     window.scrollLock.disableScrolling();
     document.addEventListener('click', onDocumentOutside);
+
   };
 
   const closeMenu = () => {
@@ -27,6 +34,7 @@ const addToggleMenu = () => {
     toggle.classList.remove(OPENED_CLASS);
     nav.classList.remove(OPENED_CLASS);
     document.removeEventListener('keydown', onDocumentKeydown);
+    nav.removeEventListener('click', onLinkClick);
     document.removeEventListener('click', isMenu);
     window.scrollLock.enableScrolling();
     document.removeEventListener('click', onDocumentOutside);
